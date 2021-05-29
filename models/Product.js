d38b48e1a2d984f9da654433f0b698fa139d8e52
Roy Kachani <roy.kachani@gmail.com>
2021-05-28 23:13:19 -0300

    se agrego propiedad photos al schema de modelo con formato array

--- models/Product.js
+++ models/Product.js
@@ -1,38 +1,42 @@
+const { array } = require('@hapi/joi');
 const { Schema, model } = require('mongoose');
 
 const ProdutSchema = Schema(
-  {
-    title: {
-      type: String,
-      require: true,
-    },
-    price: {
-      type: Number,
-      require: true,
-    },
-
-    condition: {
-      type: String,
-      default: 'new',
-    },
-    description: {
-      type: String,
-      require: true,
-    },
-    enable: {
-      type: Boolean,
-      default: true,
-    },
-    avaible_quantity: {
-      type: Number,
-      require: true,
-    },
-    sold: {
-      type: Number,
-      default: 0,
-    },
-  },
-  { timestamps: true } //crea un createAT y un updateAT automatico con date.now
+	{
+		title: {
+			type: String,
+			require: true,
+		},
+		price: {
+			type: Number,
+			require: true,
+		},
+		condition: {
+			type: String,
+			default: 'new',
+		},
+		description: {
+			type: String,
+			require: true,
+		},
+		photo: {
+			type: Array,
+			default: [],
+		},
+		enable: {
+			type: Boolean,
+			default: true,
+		},
+		avaible_quantity: {
+			type: Number,
+			require: true,
+		},
+		sold: {
+			type: Number,
+			default: 0,
+		},
+	},
+	{ timestamps: true } //crea un createAT y un updateAT automatico con date.now
 );
 
 module.exports = model('products', ProdutSchema);
