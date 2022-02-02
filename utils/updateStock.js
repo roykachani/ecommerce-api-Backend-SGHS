78fc7410f9bbebf4841bc17d67da58b84f5b4f74
Roy Kachani <roy.kachani@gmail.com>
2022-02-02 01:30:19 -0300

    implementa la baja de stock por talle

--- utils/updateStock.js
+++ utils/updateStock.js
@@ -2,11 +2,23 @@ const Product = require('../models/Product');
 
 const updateStock = async (products) => {
 	try {
+		let keyCompoused;
+		let sizeValue;
+		const sizePath = await products.map(({ sizes }) => {
+			let [size] = Object.keys(sizes);
+			[sizeValue] = Object.values(sizes);
+			keyCompoused = `sizes.$[].${size}`;
+		});
+		console.log(typeof keyCompoused, typeof sizeValue);
 		const newResulStock = products.map(({ id, quantity }) =>
 			Product.findOneAndUpdate(
 				{ _id: id },
 				{
-					$inc: { avaible_quantity: -quantity, sold: +quantity },
+					$inc: {
+						avaible_quantity: -quantity,
+						sold: +quantity,
+						[keyCompoused]: -sizeValue,
+					},
 				}
 			)
 		);
