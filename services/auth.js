2e324a41ca06b4d334a463b5b0b9793be0357756
Roy Kachani <roy.kachani@gmail.com>
2022-03-23 15:57:12 -0300

    files reader for key files to .env for heroku

--- services/auth.js
+++ services/auth.js
@@ -1,7 +1,9 @@
 const jwt = require('jsonwebtoken');
 const fs = require('fs'); //file system
-const privateKey = fs.readFileSync('./keys/private.pem');
-const publicKey = fs.readFileSync('./keys/public.pem');
+// const privateKey = fs.readFileSync('./keys/private.pem'); for production
+const privateKey = process.env.PRIVATE_KEY;
+// const publicKey = fs.readFileSync('./keys/public.pem'); for production
+const publicKey = process.env.PUBLIC_KEY;
 const singOptions = { expiresIn: '8h', algorithm: 'RS256' };
 
 const createToken = (payload) => jwt.sign(payload, privateKey, singOptions);
